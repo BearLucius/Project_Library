1c6ec0a443a2b4076b00c60c112e913316a1837a
Krapivin <tailsforhire229@gmail.com>
2023-11-29 16:32:31 +0300

    v0.8

--- src/main/java/org/example/libraryEntity/BookEntity.java
+++ src/main/java/org/example/libraryEntity/BookEntity.java
@@ -3,12 +3,22 @@ package org.example.libraryEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+
 public class BookEntity {
     private String tittle;
-    private String author;
-    private String publsiher;
+    private AuthorEntity authors;
+    private PublisherEntity publsihers;
+
+    @Override
+    public String toString() {
+        return "Название книги: " + tittle + "|Автор книги: " + authors + "|Издательство: " + publsihers;
+    }
+
+    public BookEntity() {
+    }
 }
